915c911eed822bf52267fdab3d2be283e55acf7b
Ilyos <ilyosshaydullaye212@gmail.com>
2023-08-25 14:31:18 +0500

    tugadi

---
+++ app.js
@@ -0,0 +1,74 @@
+// 1-Javob. 
+
+// Scopelar 3ga bo'linadi:
+
+// 1-Global - let n = 'Hello World'; 
+//     console.log(n);
+
+// 2-Function - funksiya.
+
+// 3-Block - if, for, while, do while, switch.
+
+
+
+
+// 2-Javob.
+
+// function num(n){
+//     for (let i=1; i <= n; i++){
+//         if(n%i==0){
+//             console.log(i);
+//         }
+//     }
+// }
+// let n = parseInt(prompt());
+// console.log(num(n));
+
+
+
+
+// 3-Javob.
+
+// function son(a){
+//     switch(a){
+//         case 1:
+//             console.log("yomon");
+//             break;
+//         case 2:
+//             console.log("qoniqarsiz");
+//             break;
+//         case 3:
+//             console.log("qoniqarli");
+//             break;
+//         case 4:
+//             console.log("Yaxshi");
+//             break;
+//         case 5:
+//             console.log("A'lo");
+//             break;
+//         default:
+//             console.log("Xato");
+//     }
+// }
+// son(5);
+
+
+// 4-Javob.
+
+// function time (seconds){
+//     var soat =parseInt(seconds/3600);
+//     var secund = seconds % 3600;
+
+//     return soat + " soat " + secund + " sekund."; 
+// }
+
+// var son = 12321;
+// var result = time(son);
+
+// console.log(result);
+
+//5-Javob.
+
+//javascript 1995-yilda yaratilgan.
+// 1995-yilda browserda boshlagan.
+//2009-yil backendi ishlagan.
